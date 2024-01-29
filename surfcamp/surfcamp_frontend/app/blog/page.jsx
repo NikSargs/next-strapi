7921a62faeaba95e047f3d00ddc072087fcef7db
NikSargs <nik.bishkek00@gmail.com>
2024-01-29 22:22:01 +0600

    update

--- surfcamp/surfcamp_frontend/app/blog/page.jsx
+++ surfcamp/surfcamp_frontend/app/blog/page.jsx
@@ -1,20 +1,63 @@
 import HighLightArticle from "../components/Blog/HighLightArticle"
+import SubscribeToNewsletter from "../components/Blog/SubscribeToNewsletter"
+import FeaturedItems from "../components/FeaturedArticles/FeaturedItems"
 
 const Page = ({ }) => {
-    const HighLightArticleData = {
-        headline: "3 tips for a super fast takeoff",
-        excerpt: (
-            <>
-                Improving your take-off phase in surfing is a fundamental step toward riding waves with more confidence and style.
+    const HighLightArticleData = [
+        {
+            headline: "3 tips for a super fast takeoff",
+            excerpt: 
+            (
+                <>
+                    Improving your take-off phase in surfing is a fundamental step toward riding waves with more confidence and style.
 
-                Improving your take-off phase is a gradual process, and it may take time to master. Be patient, stay committed to practice, and enjoy the journey of becoming a better surfer. With dedication and persistence, you'll see progress and have more enjoyable rides. Here is how:
-            </>
-        ),
-        slug: "takeoff",
-        featuredImage: "/assets/hero-experience.png"
-    }
+                    Improving your take-off phase is a gradual process, and it may take time to master. Be patient, stay committed to practice, and enjoy the journey of becoming a better surfer. With dedication and persistence, you'll see progress and have more enjoyable rides. Here is how:
+                </>
+            ),
+            slug: "takeoff",
+            featuredImage: "/assets/hero-experience.png"},
+            {headline: "3 tips for a super fast takeoff",
+            excerpt: (
+                <>
+                    Improving your take-off phase in surfing is a fundamental step toward riding waves with more confidence and style.
+
+                    Improving your take-off phase is a gradual process, and it may take time to master. Be patient, stay committed to practice, and enjoy the journey of becoming a better surfer. With dedication and persistence, you'll see progress and have more enjoyable rides. Here is how:
+                </>
+            ),
+            slug: "takeoff",
+            featuredImage: "/assets/hero-experience.png"
+        }
+    ]
+    const featuredArticles = [
+        {
+            headline: "surfboard shaping and design behind the scenes of crafting the perfect board ",
+            slug: '/blog/whatever',
+            date: "Monday, June 05, 2023",
+            featuredImage: "assets/hero-experience.png"
+        },
+        {
+            headline: "surfboard shaping and design behind the scenes of crafting the perfect board ",
+            slug: '/blog/whatever',
+            date: "Monday, June 05, 2023",
+            featuredImage: "assets/hero-experience.png"
+        },
+        {
+            headline: "surfboard shaping and design behind the scenes of crafting the perfect board ",
+            slug: '/blog/whatever',
+            date: "Monday, June 05, 2023",
+            featuredImage: "assets/hero-experience.png"
+        },
+        {
+            headline: "surfboard shaping and design behind the scenes of crafting the perfect board ",
+            slug: '/blog/whatever',
+            date: "Monday, June 05, 2023",
+            featuredImage: "assets/hero-experience.png"
+        }
+    ]
     return <main className="blog-page">
         <HighLightArticle data={HighLightArticleData} />
+        <SubscribeToNewsletter />
+        <FeaturedItems items={featuredArticles}/>
     </main>
 }
 
--- surfcamp/surfcamp_frontend/app/components/Blog/SubscribeToNewsletter.jsx
+++ surfcamp/surfcamp_frontend/app/components/Blog/SubscribeToNewsletter.jsx
@@ -1,9 +1,48 @@
 "use client"
+
+import { useState } from "react"
+
 const SubscribeToNewsletter = ({ }) => {
+    const [email, setEmail] = useState("");
+    const [hasSignedUp, setHasSignedUp] = useState(false);
+    const onChange = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("pressed")
+        if(email.length) {
+            setHasSignedUp(true);
+        }
+    }
+    
+
     return (
-        <div>
-            <div className="newsletter"></div>
-        </div>
+        <section className="newsletter">
+            {hasSignedUp ? (
+                <h4 className="newsletter_thanks">Thank you for signing up</h4>
+            ) : (
+            <>
+                <div className="newsletter__info">
+                    <h4>subscribe to our newsletter</h4>
+                    <p className="copy">
+                    Unlock Exclusive Insights and Stay In the Know – Subscribe to Our Newsletter Today to always stay in touch
+                    </p>
+                </div>
+                <form action="" className="newsletter__form" onSubmit={onSubmit}>
+                    <input type="text" 
+                    className="newsletter__email input" 
+                    placeholder="Enter your email address"
+                    value={email}
+                    onChange={onChange}/>
+                    <button type="submit" className="newsletter__subscribe btn btn--medium btn--turquoise">SUBSCRIBE</button>
+                </form>
+            </>)
+            
+            }
+        </section>
     )
 }
 
